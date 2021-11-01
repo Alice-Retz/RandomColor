70392a76fdeb5db246a870b6eb6aebf65985cbfd
Alice-Retz <alicegretz@gmail.com>
2021-11-01 18:26:40 -0500

    fixed some external issues

--- src/containers/RandomColor.jsx
+++ src/containers/RandomColor.jsx
@@ -1,1 +1,9 @@
 import React, { Component } from 'react';
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+export default class ColorRandomizer extends Component {
+  state = {
+    color: '',
+  };
+}
